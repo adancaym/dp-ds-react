988d7b12cb42c94ea2cc98c3809ef89d235824d0
adancaym <adancaym@hotmail.com>
2023-09-23 01:23:08 -0600

    storybook

--- src/playground/App.tsx
+++ src/playground/App.tsx
@@ -1,6 +1,32 @@
-import { Container } from "src/components";
-import { Typography } from "src/components/quarks";
-import { withNavigation } from "../components/hoc/withNavigation";
+import { Container, Typography, withNavigation } from "src/components";
+import {createContext, ReactNode, useContext, useState} from "react";
+
+export interface SidebarContextProps {
+  open: boolean
+  setOpen: (_:boolean)=> void
+}
+
+export const SidebarContext = createContext<SidebarContextProps>({
+  open: false,
+  setOpen: (_:boolean)=>{}
+})
+
+export const useSidebarContext = () => useContext(SidebarContext)
+
+export const { Provider} = SidebarContext
+
+export interface SidebarProps {
+  children: ReactNode[]
+}
+
+export const Sidebar = ({children}: SidebarProps) => {
+  const [open,setOpen] = useState<boolean>(false)
+
+  return <Provider value={ { open, setOpen } }>
+    {children}
+  </Provider>
+
+}
 
 const Component = () => (
   <>
