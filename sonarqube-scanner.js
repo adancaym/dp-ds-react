e8cfd9dada8db977a50dec321efcc1bf9f61e6c0
adancaym <adancaym@hotmail.com>
2023-09-27 22:32:25 -0600

    fix: add git ignore

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -4,6 +4,17 @@ scanner(
   {
     serverUrl: "http://localhost:9000",
     token: "squ_82d0e1c7a51686c5febccb3beb8ba0168d913e53",
+    exclusions: "**/*.test.tsx",
+    tests: "./src",
+    test: {
+      inclusions: "**/*.test.tsx,**/*.test.ts",
+    },
+    typescript: {
+      lcov: {
+        reportPaths: "coverage/lcov.info",
+      },
+    },
+    testExecutionReportPaths: "test-report.xml",
     options: {
       "sonar.sources": "./src",
     },
--- src/components/molecules/navigation/Navigation.tsx
+++ src/components/molecules/navigation/Navigation.tsx
@@ -1,26 +1,30 @@
-import { Children, FC } from 'react';
-import { Container, INavigationItem, INavigationProps, IPositionVariant, withBrowserRouter, withThemeContext } from 'src/components';
-import { MakeNavigationTheme, useTheme } from 'src/theme';
+import { Children, FC } from "react";
+import {
+  Container,
+  INavigationItem,
+  INavigationProps,
+  IPositionVariant,
+  withBrowserRouter,
+  withThemeContext,
+} from "src/components";
+import { MakeNavigationTheme, useTheme } from "src/theme";
 
-import { Section } from './components/Section';
+import { Section } from "./components/Section";
+import { IComponent } from "src/types";
 
-let Navigation: FC<INavigationProps> = ({ children = [], router }) => {
+interface INavigationElementProps extends IComponent {
+  children: INavigationItem[];
+}
+
+const NavigationElement: FC<INavigationElementProps> = ({ children }) => {
   const positions = ["left", "center", "right"] as IPositionVariant[];
 
   const { theme } = useTheme();
+
   const { toolbar, section } = MakeNavigationTheme(theme);
 
-  const routes = Children.map( children, ({ props }: INavigationItem) => props).filter(({ path }) => path);
-  const allRoutesExist = routes.every(({ path }) => router.map(({ path: routePath }) => routePath).includes(path));
-
-  const missingRoutes = routes.filter(({ path }) => !router.map(({ path: routePath }) => routePath).includes(path));
-
-  const message = `%cMissing routes: ${missingRoutes.map(({ path }) => path).join(", ")}`;
-
-  if (!allRoutesExist) console.log(message, "color: red; font-size: 1.2rem");
-
-  let NavigationElement: FC = () => (
-    <Container style={toolbar} role='navigation'>
+  return (
+    <Container style={toolbar} role="navigation">
       {positions.map((position) => (
         <Section
           key={position}
@@ -32,11 +36,25 @@ let Navigation: FC<INavigationProps> = ({ children = [], router }) => {
       ))}
     </Container>
   );
-  NavigationElement = withBrowserRouter({ Component: NavigationElement });
-  NavigationElement = withThemeContext({ Component: NavigationElement });
-  return <NavigationElement />;
 };
 
+let Navigation: FC<INavigationProps> = ({ children = [], router }) => {
 
+  const routes = Children.map( children, ({ props }: INavigationItem) => props ).filter(({ path }) => path);
+
+  const allRoutesExist = routes.every(({ path }) => router.map(({ path: routePath }) => routePath).includes(path));
+
+  const missingRoutes = routes.filter( ({ path }) => !router.map(({ path: routePath }) => routePath).includes(path) );
+
+  const message = `%cMissing routes: ${missingRoutes.map(({ path }) => path).join(", ")}`;
+
+  if (!allRoutesExist) console.log(message, "color: red; font-size: 1.2rem");
+
+  let Element = withThemeContext({ Component: NavigationElement });
+
+  Element = withBrowserRouter({ Component: Element });
+
+  return <Element children={children} />;
+};
 
 export default Navigation;
